3b2a0472c34ff66c7c7af556da087aa4c77ff8b2
Alexandr Chernyakov <chernyakovit@gmail.com>
2019-10-14 15:08:37 +0300

    upgrade perfomance of dimension context

--- src/components/GridItem.js
+++ src/components/GridItem.js
@@ -1,23 +1,15 @@
-import React, { useContext, useRef, useEffect, useCallback } from "react";
+import React, { useContext } from "react";
 import { GiRocket } from "react-icons/gi";
 import fuelContext from "../context/fuel";
 import dronPositionContext from "../context/dronePosition";
 
 export default function GridItem({ drone }) {
   const { fuel } = useContext(fuelContext);
-  const { setDimensions } = useContext(dronPositionContext);
-  const dronEl = useRef(null);
+  const {measuredRef } = useContext(dronPositionContext);
   const color = fuel ? "orange" : "grey";
-  const set = useCallback(
-    () => setDimensions(dronEl.current.offsetTop, dronEl.current.offsetLeft),
-    [setDimensions]
-  );
-  useEffect(() => {
-    dronEl.current && set();
-  }, [set]);
   if (drone) {
     return (
-      <div className="gridItem drone" ref={dronEl}>
+      <div className="gridItem drone" ref={measuredRef}>
         <GiRocket className="droneIcon" style={{ color }} />
       </div>
     );
